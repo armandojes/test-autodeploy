e827e6f352d3f6ada96c6cd4aedb65fa98931526
armandojes <armandodejesus678@gmail.com>
2022-12-02 11:58:07 -0600

    update

--- source/index.tsx
+++ source/index.tsx
@@ -5,7 +5,6 @@ import { createRoot } from 'react-dom/client';
 import App from './app/index';
 
 console.log('PUBLIC_PATH', process.env.PUBLIC_PATH);
-console.log('APP', process.env.APP);
 
 const root = createRoot(
   document.getElementById('render_target') as HTMLElement
