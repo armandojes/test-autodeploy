9b75783ee7ffac2619242f48dc96e234e2ad82d5
armandojes <armandodejesus678@gmail.com>
2022-12-02 11:53:26 -0600

    update env

--- source/index.tsx
+++ source/index.tsx
@@ -1,6 +1,11 @@
+// @ts-nocheck
+
 import { createRoot } from 'react-dom/client';
 
 import App from './app/index';
+
+console.log('PUBLIC_PATH', process.env.PUBLIC_PATH);
+console.log('APP', process.env.APP);
 
 const root = createRoot(
   document.getElementById('render_target') as HTMLElement
--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,65 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+const dotenv = require('dotenv');
+const webpack = require('webpack');
 
-const getConfig = (envs) => ({
-  entry: path.resolve(__dirname, './source/index.tsx'),
-  output: {
-    path: path.resolve(__dirname, './build'),
-    filename: 'app.js',
-    publicPath: 'http://localhost:3000/',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx|ts|tsx)$/i,
-        exclude: /(node_modules)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              '@babel/preset-env',
-              ['@babel/preset-react', { runtime: 'automatic' }],
-              '@babel/preset-typescript',
-            ],
-            plugins: [
-              envs.env === 'development' && 'react-refresh/babel',
-            ].filter(Boolean),
+const getConfig = (envs) => {
+  dotenv.config({ path: path.resolve(__dirname, `./${envs.env}.env`) });
+
+  return {
+    entry: path.resolve(__dirname, './source/index.tsx'),
+    output: {
+      path: path.resolve(__dirname, './build'),
+      filename: 'app.js',
+      publicPath: process.env.PUBLIC_PATH,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx|ts|tsx)$/i,
+          exclude: /(node_modules)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                '@babel/preset-env',
+                ['@babel/preset-react', { runtime: 'automatic' }],
+                '@babel/preset-typescript',
+              ],
+              plugins: [
+                envs.env === 'development' && 'react-refresh/babel',
+              ].filter(Boolean),
+            },
           },
         },
-      },
-      {
-        test: /\.(png|jpe?g|gif)$/i,
-        use: {
-          loader: 'file-loader',
-          options: {
-            publicPath: 'http://localhost:3000/',
+        {
+          test: /\.(png|jpe?g|gif)$/i,
+          use: {
+            loader: 'file-loader',
+            options: {
+              publicPath: process.env.PUBLIC_PATH,
+            },
           },
         },
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, './source/index.html'),
-    }),
-    envs.env === 'development' && new ReactRefreshWebpackPlugin(),
-  ].filter(Boolean),
-  resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx'],
-  },
-  mode: envs.env === 'production' ? 'production' : 'development',
-  devServer: {
-    port: 3000,
-    hot: true,
-  },
-});
+      ],
+    },
+    plugins: [
+      new webpack.EnvironmentPlugin(['APP', 'PUBLIC_PATH']),
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, './source/index.html'),
+      }),
+      envs.env === 'development' && new ReactRefreshWebpackPlugin(),
+    ].filter(Boolean),
+    resolve: {
+      extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    },
+    mode: envs.env === 'production' ? 'production' : 'development',
+    devServer: {
+      port: 3000,
+      hot: true,
+    },
+  };
+};
 
 module.exports = getConfig;
